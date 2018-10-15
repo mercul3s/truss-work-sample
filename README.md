a33875f08f70d0b39e63b452a786fd51a44fd096
Mercedes Coyle <me@mercedescoyle.com>
2018-10-14 21:13:12 -0700

    Added readme
    Signed-off-by: Mercedes Coyle <me@mercedescoyle.com>

---
+++ README.md
@@ -0,0 +1,21 @@
+## CSV Normalization Work Sample
+
+## Build and Run this sample
+### Option 1:
+* download and run a [release](https://github.com/mercul3s/truss-work-sample/releases) appropriate
+for your OS. Builds are available for Linux, Windows, and macOS. 
+
+### Option 2:
+* Clone this repo, build, and run locally.
+* You will need to have [go](https://golang.org/doc/install) installed on your
+machine. This codebase uses standard libraries, and should be compatible with go
+1.x versions.
+* run `go build .` to compile a binary called `work_sample`.
+* run the binary on the command line, providing it a path to the file you want
+to normalize: 
+```
+$ ./work_sample samples/sample.csv
+finished normalizing input file, writing to normalized_sample.csv
+```
+* Once the file has been processed, you can view the results in the normalized
+csv file.
--- normalize.go
+++ normalize.go
@@ -11,47 +11,6 @@ import (
 	"github.com/mercul3s/work_sample/format"
 )
 
-/*
-
-Please write a tool that reads a CSV formatted file on `stdin` and
-emits a normalized CSV formatted file on `stdout`. Normalized, in this
-case, means:
-
-* The entire CSV is in the UTF-8 character set.
-* The Timestamp column should be formatted in ISO-8601 format.
-* The Timestamp column should be assumed to be in US/Pacific time;
-  please convert it to US/Eastern.
-* All ZIP codes should be formatted as 5 digits. If there are less
-  than 5 digits, assume 0 as the prefix.
-* All name columns should be converted to uppercase. There will be
-  non-English names.
-* The Address column should be passed through as is, except for
-  Unicode validation. Please note there are commas in the Address
-  field; your CSV parsing will need to take that into account. Commas
-  will only be present inside a quoted string.
-* The columns `FooDuration` and `BarDuration` are in HH:MM:SS.MS
-  format (where MS is milliseconds); please convert them to a floating
-  point seconds format.
-* The column "TotalDuration" is filled with garbage data. For each
-  row, please replace the value of TotalDuration with the sum of
-  FooDuration and BarDuration.
-* The column "Notes" is free form text input by end-users; please do
-  not perform any transformations on this column. If there are invalid
-  UTF-8 characters, please replace them with the Unicode Replacement
-  Character.
-
-You can assume that the input document is in UTF-8 and that any times
-that are missing timezone information are in US/Pacific. If a
-character is invalid, please replace it with the Unicode Replacement
-Character. If that replacement makes data invalid (for example,
-because it turns a date field into something unparseable), print a
-warning to `stderr` and drop the row from your output.
-
-You can assume that the sample data we provide will contain all date
-and time format variants you will need to handle.
-
-*/
-
 var fileName string
 
 func main() {
@@ -76,8 +35,6 @@ func main() {
 		handleFileError(err, fileName)
 	}
 
-	fmt.Println("CSV Output filename: ", normFileName)
-
 	reader := csv.NewReader(bufio.NewReader(csvIn))
 	writer := csv.NewWriter(csvOut)
 
@@ -86,6 +43,7 @@ func main() {
 		row, err := reader.Read()
 		if err == io.EOF {
 			// flush the writer and exit
+			fmt.Printf("finished normalizing input file, writing to %s\n", normFileName)
 			writer.Flush()
 			os.Exit(0)
 		}
@@ -98,6 +56,7 @@ func main() {
 			fmt.Printf("Unable to normalize row %s due to error %s - skipping\n", row, err)
 			continue
 		}
+
 		writer.Write(normRow)
 	}
 }
